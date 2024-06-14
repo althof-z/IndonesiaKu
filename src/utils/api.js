714be993db91e61f160ddc5b36c2d3335724e513
althof-z <althof.zpv@gmail.com>
2024-06-14 22:06:29 +0700

    add firebase

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+const api = (() => {
+  function putAccessToken(accessToken) {
+    localStorage.setItem('accessToken', accessToken);
+  }
+
+  function getAccessToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  return {
+    putAccessToken,
+    getAccessToken,
+  };
+})();
+
+export default api;
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAO-bznIPtd2MUX71YSB2wc40sji18Xj_w",
+  authDomain: "indonesiaku-b84d2.firebaseapp.com",
+  projectId: "indonesiaku-b84d2",
+  storageBucket: "indonesiaku-b84d2.appspot.com",
+  messagingSenderId: "945312676316",
+  appId: "1:945312676316:web:723586048f86de94a77719",
+  measurementId: "G-XM7Q59PY5Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
