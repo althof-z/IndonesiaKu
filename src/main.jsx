fd869245db0ecceba8abd3870925b566a04babba
yondraseptian <yondraseptian@gmail.com>
2024-06-14 20:55:09 +0700

    menambah page destinasi

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-// import './styles/animate.css';
-// import './styles/swiper-bundle.min.css';
 import './styles/tailwind.css';
+import './styles/animate.css';
+import './styles/swiper-bundle.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
         <App />
-      </React.StrictMode>
-    </BrowserRouter>
+    </React.StrictMode>
+  </BrowserRouter>,
 );
