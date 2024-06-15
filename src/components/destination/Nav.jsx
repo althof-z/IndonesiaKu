40ac63569a871252efa4e3bbd694b56fd5b257a2
yondraseptian <yondraseptian@gmail.com>
2024-06-15 16:14:30 +0700

    menambah detailpage dan wishlistpage

--- src/components/destination/Nav.jsx
+++ src/components/destination/Nav.jsx
@@ -7,22 +7,9 @@ export default function Navigation() {
         <h1>IndonesiaKu</h1>
       </Link>
       <div>
-        <ul className="flex gap-4">
-          <li>
-            <Link to="/login">
-              <button className="text-primary bg-white px-4 py-2 rounded-lg">
-                Login
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/register">
-              <button className="text-primary bg-white px-4 py-2 rounded-lg">
-                register
-              </button>
-            </Link>
-          </li>
-        </ul>
+        <Link to="/wishlist">
+          <button className="text-primary bg-white px-4 py-2 rounded-lg">wishlist</button>
+        </Link>
       </div>
     </div>
   );
